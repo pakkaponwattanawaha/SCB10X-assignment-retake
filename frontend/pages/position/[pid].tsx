d1e199538d7bc13ba4808a444beb85826b8bdd9e
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-06-08 21:26:58 +0700

    feat-frontend: add price oracle and event listener

--- frontend/pages/position/[pid].tsx
+++ frontend/pages/position/[pid].tsx
@@ -2,7 +2,13 @@ import React, { useEffect, useState } from "react";
 import Router from "next/router";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { LENDING_FACTORY, FACTORY_ABI } from "contract";
+import {
+  LENDING_FACTORY,
+  FACTORY_ABI,
+  PRICEORACLE_ABI,
+  PRICE_ORACLE,
+  DAI,
+} from "contract";
 import { useLendingPosition } from "hooks/useLendingPosition";
 import { Loading } from "web3uikit";
 import { MoralisProvider, useChain, useMoralis } from "react-moralis";
@@ -22,6 +28,7 @@ const Pid: NextPage = () => {
   const { chain } = useChain();
   const { fetchPosition, position, isLoading } = useLendingPosition();
   const [balance, setBalance] = useState<String>("");
+  const [daiPrice, setDaiPrice] = useState<Number>();
 
   const { doTx, clearTx } = useTx();
 
@@ -52,7 +59,18 @@ const Pid: NextPage = () => {
     };
     await doTx(sendOptions);
   };
-
+  const getCurrentDAIPrice = async () => {
+    const priceOracleContract = new ethers.Contract(
+      PRICE_ORACLE[chain.networkId],
+      PRICEORACLE_ABI,
+      moralisProvider
+    );
+    const daiPrice = (
+      await priceOracleContract.functions.getAssetPrice(DAI[chain.networkId])
+    )[0];
+    setDaiPrice(Number(daiPrice));
+    return daiPrice;
+  };
   const getBalance = async () => {
     const balance = moralisProvider
       ? (await moralisProvider.getBalance(position.address)).toString()
@@ -60,20 +78,18 @@ const Pid: NextPage = () => {
     setBalance(ethers.utils.formatEther(balance));
   };
   useEffect(() => {
-    getBalance();
-    console.log(position);
-  }, [pid, position]);
+    if (position && position?.address) getBalance();
+  }, [position]);
 
   useEffect(() => {
     fetchPosition(Number(pid));
     if (isWeb3Enabled && chain && position) {
+      getCurrentDAIPrice();
       positionListener();
     } else {
       enableWeb3();
     }
-  }, [chain, isWeb3Enabled]);
-
-  ///// add close position event listener => refresh to use the isOpen boolean to handle///////
+  }, [pid, chain, isWeb3Enabled]);
 
   const positionListener = () => {
     const positionContract = new ethers.Contract(
@@ -84,7 +100,7 @@ const Pid: NextPage = () => {
     positionContract.on("closePosition", (positionAddress, owner) => {
       if (account?.toUpperCase() === owner?.toUpperCase()) {
         clearTx();
-        Router.reload();
+        // Router.reload();
       }
     });
   };
@@ -104,6 +120,7 @@ const Pid: NextPage = () => {
           <div>
             <h3>You are the owner of this position</h3>
             <div>{balance.toString()}</div>
+            <div>DAI price :{10 ** 18 / daiPrice}</div>
             <button
               onClick={(e) => handleClosePosition(e)}
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
