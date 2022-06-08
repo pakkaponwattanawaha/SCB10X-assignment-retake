6bb74ed0109e7a0480d43069408dcd2cf5e5ddd3
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-06-08 16:47:05 +0700

    feat-frontend:add Dashboard page

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import type { NextPage } from "next";
+import { useLendingFactory } from "hooks/useLendingFactory";
+import { Loading } from "web3uikit";
 
 const Dashboard: NextPage = () => {
-  return <div>dashboard</div>;
+  const { allPositionOpened, isLoading } = useLendingFactory();
+  return (
+    <div>
+      dashboard
+      {isLoading ? (
+        <Loading size={40} spinnerColor="#0A0A0A" />
+      ) : (
+        <>{allPositionOpened.toString()}</>
+      )}
+    </div>
+  );
 };
 export default Dashboard;
