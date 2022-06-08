5501877b19d55f5009a3fd710b3f28fcf0ef70e7
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-06-09 03:15:13 +0700

    feat-frontend: styling position page

--- frontend/pages/position/[pid].tsx
+++ frontend/pages/position/[pid].tsx
@@ -3,18 +3,25 @@ import Router from "next/router";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import {
-  LENDING_FACTORY,
-  FACTORY_ABI,
   PRICEORACLE_ABI,
   PRICE_ORACLE,
   DAI,
+  CHAINID_TO_DETAIL,
 } from "contract";
 import { useLendingPosition } from "hooks/useLendingPosition";
 import { Loading } from "web3uikit";
-import { MoralisProvider, useChain, useMoralis } from "react-moralis";
+import { useChain, useMoralis } from "react-moralis";
 import { POSITION_ABI } from "contract";
 import { useTx } from "context/transaction";
 import { ethers } from "ethers";
+import {
+  formatWei,
+  numberToFixedDigit,
+  openInNewTab,
+  shortenAddress,
+  statusFormatter,
+} from "utils";
+import { PositionData } from "types";
 
 const Pid: NextPage = () => {
   const router = useRouter();
@@ -28,7 +35,8 @@ const Pid: NextPage = () => {
   const { chain } = useChain();
   const { fetchPosition, position, isLoading } = useLendingPosition();
   const [balance, setBalance] = useState<String>("");
-  const [daiPrice, setDaiPrice] = useState<Number>();
+  const [daiPrice, setDaiPrice] = useState<number>();
+  const [positionData, setPositionData] = useState<PositionData>();
 
   const { doTx, clearTx } = useTx();
 
@@ -48,7 +56,25 @@ const Pid: NextPage = () => {
     console.log(additionalAmount);
     return additionalAmount;
   };
-
+  const getPositionData = async () => {
+    const positionContract = new ethers.Contract(
+      position.address,
+      POSITION_ABI,
+      moralisProvider
+    );
+    const positionData = await positionContract.functions.getPositionData();
+    let ret = {
+      totalCollateralETH: Number(positionData[0]),
+      totalDebtETH: Number(positionData[1]),
+      availableBorrowsETH: Number(positionData[2]),
+      currentLiquidationThreshold: Number(positionData[3]),
+      ltv: Number(positionData[4]),
+      healthFactor: Number(positionData[5]),
+    };
+    setPositionData(ret);
+    console.log("positionData", ret);
+    return positionData;
+  };
   const closePosition = async () => {
     const value = await getAdditionalETHToClosePosition();
     const sendOptions = {
@@ -75,10 +101,18 @@ const Pid: NextPage = () => {
     const balance = moralisProvider
       ? (await moralisProvider.getBalance(position.address)).toString()
       : "0";
-    setBalance(ethers.utils.formatEther(balance));
+    setBalance(
+      numberToFixedDigit(
+        ethers.utils.formatEther(balance).toString(),
+        8
+      ).toString()
+    );
   };
   useEffect(() => {
-    if (position && position?.address) getBalance();
+    if (position && position?.address) {
+      getBalance();
+      if (moralisProvider) getPositionData();
+    }
   }, [position]);
 
   useEffect(() => {
@@ -104,33 +138,106 @@ const Pid: NextPage = () => {
       }
     });
   };
+  const pnlInDAI =
+    position.positionPrice == 0
+      ? 0
+      : numberToFixedDigit(
+          (
+            (1 / daiPrice - 1 / position.positionPrice) *
+            position.borrowAmount
+          ).toString(),
+          8
+        );
   return (
-    <div>
-      pid:{pid}
-      <div>
-        {isLoading ? (
-          <Loading size={40} spinnerColor="#0A0A0A" />
-        ) : (
-          <>{JSON.stringify(position)}</>
-        )}
+    <div className="h-screen bg-gray-50 flex justify-center gap-2">
+      <div className="centered  rounded w-1/2  mt-10">
+        <h2 className="text-[42px] font-bold pb-3">Position Detail </h2>
+        <div>
+          {isLoading ? (
+            <Loading size={40} spinnerColor="#0022EE" />
+          ) : (
+            <div className="border bg-white shadow-xl rounded-xl p-5">
+              <div>
+                <h4 className="text-[24px] font-bold pb-3">Supply Info </h4>
+                <div className="cursor-pointer">
+                  <span>Position Address: </span>
+                  <a
+                    className="font-bold"
+                    onClick={() => {
+                      openInNewTab(
+                        CHAINID_TO_DETAIL[chain.chainId].scan +
+                          "address/" +
+                          position.address
+                      );
+                    }}
+                  >
+                    {shortenAddress(position.address)}
+                  </a>
+                </div>
+                <div>Status :{statusFormatter(position)}</div>
+                <div>Deposit Amount: {formatWei(position.amount)} ETH</div>
+                <div>
+                  Position Price:{" "}
+                  {position.positionPrice == 0
+                    ? 0
+                    : 10 ** 18 / position.positionPrice}
+                </div>
+
+                <h4 className="text-[24px] font-bold py-3">Borrow Info </h4>
+                <div>Borrow Amount: {formatWei(position.borrowAmount)} DAI</div>
+                <div>Current ETH price: {10 ** 18 / daiPrice}</div>
+                <div>
+                  Interest Rate Mode:{" "}
+                  {position.interestRateMode === 1 ? "Fixed" : "Variable"}
+                </div>
+                <div>Leverage Percentage: {position.leveragePercentage} %</div>
+
+                <div>PNL: {pnlInDAI} DAI </div>
+              </div>
+              <div className="mt-5">
+                {position &&
+                position?.owner?.toUpperCase() === account?.toUpperCase() ? (
+                  <div>
+                    <button
+                      onClick={(e) => handleClosePosition(e)}
+                      className="bg-main1 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    >
+                      Close Position
+                    </button>
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      <div>
-        {position &&
-        position?.owner?.toUpperCase() === account?.toUpperCase() ? (
-          <div>
-            <h3>You are the owner of this position</h3>
-            <div>{balance.toString()}</div>
-            <div>DAI price :{10 ** 18 / daiPrice}</div>
-            <button
-              onClick={(e) => handleClosePosition(e)}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Close Position
-            </button>
-          </div>
-        ) : (
-          <div></div>
-        )}
+      <div className="centered  rounded w-1/3  mt-10 pt-[75px]">
+        <div>
+          {!isLoading && positionData ? (
+            <div className="border bg-white shadow-xl rounded-xl p-5">
+              <h4 className="text-[24px] font-bold pb-3">Aave Details </h4>
+              <div>
+                Total Collateral:{formatWei(positionData.totalCollateralETH)}{" "}
+                ETH
+              </div>
+              <div>Total Debt: {formatWei(positionData.totalDebtETH)} ETH</div>
+              <div>
+                Available Borrows: {formatWei(positionData.availableBorrowsETH)}{" "}
+                ETH
+              </div>
+              <div>
+                Current Liquidation Treshold:{" "}
+                {positionData.currentLiquidationThreshold / 100} %
+              </div>
+              <div>Loan To Value: {positionData.ltv / 100} %</div>
+              <div>Health Factor: {positionData.healthFactor}</div>
+            </div>
+          ) : (
+            <Loading size={40} spinnerColor="#0022EE" />
+          )}
+        </div>
       </div>
     </div>
   );
