da9d67505b79a6ee73451beab393facc01c04643
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-06-09 03:12:40 +0700

    feat-frontend:styling navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
 
   const createNavLink = (label: string, endpoint: string) => {
     const activeStyle = router.pathname.endsWith(endpoint)
-      ? "text-black border-b-2 border-mainPink"
+      ? "text-black border-b-2 border-main1"
       : "text-gray-500 hover:text-black";
 
     return (
@@ -22,20 +22,19 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav className="p-5 flex flex-row items-center justify-between">
+      <nav className="bg-gray-200 p-5 flex flex-row items-center justify-between">
         <Link href={{ pathname: "/" }} replace>
           <a>
-            <div className="mb-2 flex flex-row items-center border-mainPink justify-center">
-              <h1 className="font-medium ml-2 mt-[13px] text-xl">SCB10X Dev</h1>
+            <div className="mb-2 flex flex-row items-center border-main1 justify-center">
+              <h1 className="font-medium ml-2 mt-[13px] text-xl">10XDev</h1>
             </div>
           </a>
         </Link>
+        <div className="flex flex-row items-center justify-between mt-2 ml-8">
+          {createNavLink("Lending", "/")}
+          {createNavLink("Dashboard", "/dashboard")}
+        </div>
         <div className="flex flex-row items-center">
-          <div className="flex flex-row items-center justify-between mt-2">
-            {createNavLink("Lending", "/")}
-            {createNavLink("Dashboard", "/dashboard")}
-          </div>
-
           <div className="flex flex-row py-2 px-4">
             <ConnectButton
               moralisAuth={true}
